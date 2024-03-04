67f862ab68a3b8c6053f5b749579139cc64c7046
jgreenwell <jsgreenwell83@gmail.com>
2024-03-04 13:13:26 -0600

    Added main logic

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BlackJack {
     // default values to use then remove (cause only on per deck)
@@ -7,6 +6,10 @@ public class BlackJack {
     // Ace of Spades would be Suit "Spades", Face "Ace", value 1...or 11?
     public record Card(String suit, String face, int value){}
     protected List<Card> deck = new ArrayList<>();
+
+    // Here the user's hand and the dealers (a list of card objects)
+    protected List<Card> dealerHand = new ArrayList<>();
+    protected List<Card> playerHand = new ArrayList<>();
 
     public BlackJack() {
         // build the deck
@@ -28,7 +31,83 @@ public class BlackJack {
                 }
             }
         }
+        // Randomize order of deck - they even call it shuffle
+        Collections.shuffle(deck);
 
+        // call draw card to return a new Card to add to Dealer's hand
+        // Then repeat with player so everyone has 2 starting cards
+        drawCard("dealer");
+        drawCard("dealer");
 
+        drawCard("player");
+        drawCard("player");
+    }
+
+    protected Card drawCard() {
+        /*
+          Return a random card from deck & remove it from play (deck)
+          @return random card
+         */
+        int idx = new Random().nextInt(deck.size());
+        Card temp = deck.get(idx);
+        deck.remove(idx);
+        return temp;
+    }
+
+    protected void drawCard(String who) {
+        /*
+          Wrapper function - it calls the drawcard and then adds that to the
+          person passed (dealer or player's) deck.
+
+          @param String who is drawing card (player or dealer)
+         */
+
+        switch (who.toLowerCase()) {
+            case "player":
+                playerHand.add(drawCard());
+                break;
+            case "dealer":
+                dealerHand.add(drawCard());
+                break;
+        }
+    }
+    protected String getHand(String who) {
+        // Build the hand using method chaining of appends & a join of values
+        StringBuilder hand = new StringBuilder();
+
+        if (who.equalsIgnoreCase("player")) {
+            for (var card : playerHand) {
+                hand.append("Card is: ").append(
+                        String.join(" ", card.face(), card.suit())
+                ).append("\n");
+            }
+        } else if (who.equalsIgnoreCase("dealer")) {
+            for (var card : dealerHand) {
+                hand.append("Card is: ").append(
+                        String.join(" ", card.face(), card.suit())
+                ).append("\n");
+            }
+        }
+
+        return hand.toString();
+    }
+
+    protected int getHandTotal(String who) {
+        // very simply - just for loop over hand and total the values
+        int total = 0;
+
+        if (who.equalsIgnoreCase("player")) {
+            for (var card : playerHand) {
+                // I can do this with Stream in 1 line no for but:
+                total += card.value();
+            }
+        } else if (who.equalsIgnoreCase("dealer")) {
+            for (var card : dealerHand) {
+                // I can do this with Stream in 1 line no for but:
+                total += card.value();
+            }
+        }
+
+        return total;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,80 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // Finish this up over weekend
         BlackJack blackjack = new BlackJack();
+        Scanner scan = new Scanner(System.in);
 
-        for (var c : blackjack.deck) {
-            System.out.println(c.face());
-            System.out.println(c.value());
-            System.out.println(c.suit());
-            System.out.println(c.toString());
+        System.out.println("The Dealer has:");
+        System.out.println(blackjack.getHand("dealer"));
+        System.out.println("The Player has:");
+        System.out.println(blackjack.getHand("player"));
+
+        // You'll need a regular if here not switch - why?
+        if (blackjack.getHandTotal("dealer") == 21) {
+            // check if dealer wins and exit if true
+            System.out.println("You Lose!!! Dealer has 21 - bye bye.");
+            System.exit(0);
+        }
+
+        // create main game loop here
+        String turn = "player";
+
+        while(true) {
+            System.out.println("The Dealer has:");
+            System.out.println(blackjack.getHand("dealer"));
+            System.out.println("The Player has:");
+            System.out.println(blackjack.getHand("player"));
+
+            switch (turn) {
+                case "player":
+                    if (blackjack.getHandTotal("player") == 21) {
+                        System.out.println("You won or got a draw! Let's find out!");
+                        turn = "dealer";
+                    } else if (blackjack.getHandTotal("player") <= 20) {
+                        System.out.print("Do you want to hit? (yes/no)");
+                        if (scan.nextLine().toLowerCase().startsWith("yes")) {
+                            blackjack.drawCard("player");
+                        } else {
+                            System.out.print("Do you want to stay? (yes/no)");
+                            if (scan.nextLine().toLowerCase().startsWith("yes")) {
+                                System.out.printf("Alright staying with %d\n",
+                                        blackjack.getHandTotal("player"));
+                                turn = "dealer";
+                            }
+                        }
+                    } else {
+                        System.out.println("You busted! Game over!");
+                        System.exit(0);
+                    }
+                    break;
+                case "dealer":
+                    if (blackjack.getHandTotal("dealer") == 21) {
+                        System.out.println("Dealer has 21 and is staying");
+                        turn = "exit";
+                    } else if (blackjack.getHandTotal("dealer") <= 15) {
+                        System.out.print("Dealer hitting!");
+                        blackjack.drawCard("dealer");
+                    } else if (blackjack.getHandTotal("dealer") > 21) {
+                        System.out.println("Dealer busted! You win! Congrats");
+                        System.exit(0);
+                    } else {
+                        System.out.print("Dealer staying!");
+                        turn = "exit";
+                    }
+                    break;
+            }
+
+            if (turn.equals("exit")) {
+                break;
+            }
+
+            System.out.print("Do you want to stop playing? (y/n) ");
+            if (scan.nextLine().toLowerCase().startsWith("y")) {
+                System.out.println("Okay, hope you had fun! Bye");
+                break;
+            }
         }
     }
 }
